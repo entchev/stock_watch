f64e78d098a4103a34df93c0827458f20f476ebe
entchev <micishi@gmail.com>
2020-11-09 20:02:53 +0000

    third

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -6,12 +6,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Root from '../components/root'
 import configureStore from '../store/store'
+import { login, logout, signup } from '../actions/session_actions'
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
 
   // for testing purposes
+  window.login = login
+  window.logout = logout
+  window.signup = signup
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   // testing over
